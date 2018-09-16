1aaa2b148c2a69d4e5fbb4e73ab03354124cbec4
Manuel Valle <vallerem17@gmail.com>
2018-09-17 01:21:02 +0200

    fixed age migration for Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_195608) do
+ActiveRecord::Schema.define(version: 2018_09_16_231706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "age", null: false
-    t.string "email", null: false
-    t.string "password_digest", null: false
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

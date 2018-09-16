39660e369b5bfabcc0ac8275b9f61f25ea08ca11
Manuel Valle <vallerem17@gmail.com>
2018-09-17 00:54:23 +0200

    cached node modules for heroku deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,9 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
 
-  # - fallback routes for react
-  def fallback_index_html
-    render :file => 'public/index.html'
-  end
-
   # called before every action on controllers
   before_action :authorize_request
-  
+
   attr_reader :current_user
 
   private
@@ -17,5 +12,10 @@ class ApplicationController < ActionController::API
   # Check for valid request token and return user
   def authorize_request
     @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
+
+  # - fallback routes for react
+  def fallback_index_html
+    render :file => "public/index.html"
   end
 end
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import UsersList from "./views/user/UsersList";
 
@@ -14,9 +14,21 @@ export class App extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={() => <h1>Home</h1>} />
+          <Route
+            exact
+            path="/"
+            component={() => (
+              <h1>
+                Home - <Link to="/list/users"> To users</Link>
+              </h1>
+            )}
+          />
           <Route exact path="/list/users" component={UsersList} />
-          <Route exact path="/list/users/:id" component={() => <h1>Detal!!</h1>} />
+          <Route
+            exact
+            path="/list/users/:id"
+            component={() => <h1>Detal!!</h1>}
+          />
           <Route path="/auth/login" render={() => <h1> Login</h1>} />
           <Route path="/auth/signup" render={() => <h1> Signup</h1>} />
           <Route render={() => <h1> Not found</h1>} />

e3702b8dedf334b2161d68f11aaa94971c305cab
Manuel Valle <vallerem17@gmail.com>
2018-09-17 00:09:49 +0200

    changed react router routes

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,10 @@ export class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={() => <h1>Home</h1>} />
-          <Route exact path="/users" component={UsersList} />
-          <Route exact path="/users/:id" component={() => <h1>Detal!!</h1>} />
-          <Route path="/login" render={() => <h1> Login</h1>} />
-          <Route path="/signup" render={() => <h1> Signup</h1>} />
+          <Route exact path="/list/users" component={UsersList} />
+          <Route exact path="/list/users/:id" component={() => <h1>Detal!!</h1>} />
+          <Route path="/auth/login" render={() => <h1> Login</h1>} />
+          <Route path="/auth/signup" render={() => <h1> Signup</h1>} />
           <Route render={() => <h1> Not found</h1>} />
         </Switch>
       </Router>
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  scope module: :v2, constraints: ApiVersion.new('v2') do
+  scope module: :v2, constraints: ApiVersion.new("v2") do
     resources :users, only: :index
   end
-  
-  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+
+  scope module: :v1, constraints: ApiVersion.new("v1", true) do
     resources :users
     post "signup", to: "users#create"
   end
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   post "login", to: "authentication#authenticate"
 
   # - react-router needs this
-  get '*path', to: "application#fallback_index_html", constraints: -> (request) do
-    !request.xhr? && request.format.html?
-  end
+  get "*path", to: "application#fallback_index_html", constraints: -> (request) do
+                 !request.xhr? && request.format.html?
+               end
 end

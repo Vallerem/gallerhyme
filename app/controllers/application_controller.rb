59f03d9d80043852ab72b320348c9238d2a6fd4d
Manuel Valle <vallerem17@gmail.com>
2018-09-18 01:06:06 +0200

    make react router work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+
   include Response
   include ExceptionHandler
 
@@ -16,6 +18,8 @@ class ApplicationController < ActionController::API
 
   # - fallback routes for react
   def fallback_index_html
-    render :file => "public/index.html"
+    respond_to do |format|
+      format.html { render body: Rails.root.join("public/index.html").read }
+    end
   end
 end

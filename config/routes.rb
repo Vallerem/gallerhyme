20513d3d338f4af32aaedfcfeee1d9964819625e
Manuel Valle <vallerem17@gmail.com>
2018-09-18 14:33:14 +0200

    original state

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  # scope module: :v2, constraints: ApiVersion.new('v2') do
-  #   resources :users, only: :index
-  # end
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :users, only: :index
+  end
   
-  # scope module: :v1, constraints: ApiVersion.new('v1', true) do
-  #   resources :users
-  #   post "signup", to: "users#create"
-  # end
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :users
+    post "signup", to: "users#create"
+  end
 
-  resources :users
-
-  # post "login", to: "authentication#authenticate"
+  post "login", to: "authentication#authenticate"
 
   # - react-router needs this
   get '*path', to: "application#fallback_index_html", constraints: -> (request) do

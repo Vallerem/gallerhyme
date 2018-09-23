32dab5c7e81d11a380693f94a6e862c7031ace21
Manuel Valle <vallerem17@gmail.com>
2018-09-23 21:00:31 +0200

    added CHOKIDAR_USEPOLLING to reacts .env for poll enabled webpack hot reloading

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ export class App extends Component {
             path="/"
             component={() => (
               <h1>
-                Home <Link to="/users"> To users</Link>
+                Home jajajaj <Link to="/users"> To users</Link>
               </h1>
             )}
           />
